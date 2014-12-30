532b5fecda8dae7f0f74cbac6d2c7b55b6cef21b
Yamila Moreno <yamila.ms@gmail.com>
2014-12-30 17:30:50 +0100

    Fix style to be clearer, thanks to @lmivan

--- FizzBuzz.groovy
+++ FizzBuzz.groovy
@@ -2,11 +2,11 @@ class FizzBuzz {
 
     def check(Integer num) {
 
-        if(num%3==0 && num%5==0) {
+        if(num % 3 == 0 && num % 5 == 0) {
             return 'fizzbuzz'
-        } else if(num%3==0) {
+        } else if(num % 3 == 0) {
             return 'fizz'
-        } else if (num%5==0) {
+        } else if (num % 5 == 0) {
             return 'buzz'
         } else {
             return num
